0084ef20f284be2d92ddc0d1a6aa17b298c3490d
Esteba <esteba@gmx.de>
2015-02-07 22:15:03 +0100

    Javascript-Funktionen zum Platzieren von Booten und Schießen

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -45,12 +45,12 @@ object Application extends Controller {
       Ok(views.html.game("Das Spiel kann beginnen!! \n "
         + "Spieler 1 : " + arrayOfGames(0).players(0).playerName
         + " Spieler 2 : " + arrayOfGames(0).players(1).playerName
-        + " Game ID: " + arrayOfGames(0).id, arrayOfGames(0)),request.session)
+        + " Game ID: " + arrayOfGames(0).id, arrayOfGames(0)))
     }else if (userID == (arrayOfGames(0).players(0).playerName) || userID == (arrayOfGames(0).players(1).playerName)){
       Ok(views.html.game("Das Spiel kann beginnen!! \n "
         + "Spieler 1 : " + arrayOfGames(0).players(0).playerName
         + " Spieler 2 : " + arrayOfGames(0).players(1).playerName
-        + " Game ID: " + arrayOfGames(0).id, arrayOfGames(0)),request.session)
+        + " Game ID: " + arrayOfGames(0).id, arrayOfGames(0)))
     }else{
       arrayOfPlayers(0) = null
       arrayOfPlayers(1) = null
